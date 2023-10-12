61868f57443688457e6836e95d8f4b6afaef931b
bbeluco <beluco.bispo@hotmail.com>
2023-10-12 11:45:13 -0300

    Unit test BetSlip 'should call expected functions' created

--- src/components/betslip/BetSlip.test.tsx
+++ src/components/betslip/BetSlip.test.tsx
@@ -1,7 +1,7 @@
 import BetSlip from "./BetSlip";
 import renderer from 'react-test-renderer';
 import { betOptionMock, pushSelectedOptionInfoMock, navigator } from "../../mocks/mockPropsComponents";
-
+import { fireEvent, render, screen } from "@testing-library/react-native";
 
 describe('<BetSlip />', () => {
     it('should render UI correctly', () => {
@@ -13,5 +13,23 @@ describe('<BetSlip />', () => {
             />
         ).toJSON();
         expect(component).toMatchSnapshot();
+    })
+
+    it('should call expected functions', () => {
+        const spyPushSelectedOptionInfoMock = jest.spyOn(pushSelectedOptionInfoMock, 'remove');
+        const spyNavigator = jest.spyOn(navigator, 'onClose')
+
+        render(
+            <BetSlip 
+                betOptions={betOptionMock}
+                removeItemFromArray={pushSelectedOptionInfoMock.remove}
+                closeDrawer={navigator.onClose}
+            />
+        )
+
+        fireEvent.press(screen.getByText('DELETE'))
+        expect(spyPushSelectedOptionInfoMock).toHaveBeenCalled()
+        fireEvent.press(screen.getByText('X'))
+        expect(spyNavigator).toHaveBeenCalled()
     })
 })
