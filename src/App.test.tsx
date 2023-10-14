009f9ac6fa991d17c22906bccd37dd231412bea3
bbeluco <beluco.bispo@hotmail.com>
2023-10-13 21:53:34 -0300

    All user flux app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -18,4 +18,17 @@ describe('<App />', () => {
         });
         expect(screen.toJSON()).toMatchSnapshot()
     })
+
+    it('should interact with any option available, delete it from BetSlip and restore button to default color', async () => {
+        render(<App />)
+
+        await waitFor(() => {
+            expect(screen.getByTestId(`button_Real Madrid_betOption`)).toBeTruthy()
+        });
+
+        fireEvent.press(screen.getByTestId(`button_Real Madrid_betOption`))
+        fireEvent.press(screen.getByText('DELETE'))
+        const componentProps = screen.getByTestId(`button_Real Madrid_betOption`).props
+        expect(componentProps['style']).not.toMatchObject({ backgroundColor: 'green' })
+    })
 })
